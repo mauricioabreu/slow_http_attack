ca0f7dcd002897b229874bd8baff6016faf4747e
Maurício Antunes <mauricio.abreua@gmail.com>
2022-04-20 07:57:05 -0300

    Fix go timer and comment NGINX fix

--- main.go
+++ main.go
@@ -10,37 +10,40 @@ import (
 )
 
 func main() {
+	var conn net.Conn
+	var err error
+	var timerChan <-chan time.Time
+	var timer *time.Timer
+	timer = time.NewTimer(10 * time.Second)
+	timerChan = timer.C
 
 loop:
 	for {
-		conn, err := net.Dial("tcp", "localhost:8080")
+		if conn != nil {
+			conn.Close()
+		}
+
+		conn, err = net.Dial("tcp", "localhost:8080")
 		if err != nil {
 			log.Fatal().Err(err).Msg("")
 		}
-
-		var timerChan <-chan time.Time
-		var timer *time.Timer
-		timer = time.NewTimer(10 * time.Second)
-		timerChan = timer.C
 
 		fmt.Fprint(conn, "POST / HTTP/1.1\r\n")
 		for {
 			select {
 			case <-time.After(1 * time.Second):
-				if timer != nil {
-					timer.Reset(10 * time.Second)
-				}
 				log.Info().Msg("sending body...")
-				_, err := fmt.Fprintf(conn, "a")
-				if err != nil {
+				if _, err := fmt.Fprintf(conn, "a"); err != nil {
 					log.Error().Err(err).Msg("")
 					continue loop
 				}
 			case <-timerChan:
+				timer.Reset(10 * time.Second)
 				fmt.Fprintf(conn, "\r\n\r\n")
 				ioutil.ReadAll(conn)
 				log.Info().Msg("closing connection...")
 				conn.Close()
+				continue loop
 			}
 		}
 	}
